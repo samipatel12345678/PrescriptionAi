815c0552b3396f9501ae534775aafcbe02de9464
samipatel404 <155371078+samipatel12345678@users.noreply.github.com>
2025-08-24 15:18:46 +0530

    Update DocumentController.js

--- controllers/DocumentController.js
+++ controllers/DocumentController.js
@@ -3,7 +3,6 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 import { supabase } from "../SupabaseClient.js";
-";
 import pdfParse from "pdf-parse";
 import mammoth from "mammoth";
 import OpenAI from "openai";
